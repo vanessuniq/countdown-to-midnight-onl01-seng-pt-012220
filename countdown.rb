7faef6af0bc9b0eac6ac107d14c5569ee71e04be
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-01-29 04:32:14 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
 #write your code here
 
-def countdown
+def countdown number
+  while number > 0 
+    number -= 1 
+  end 
+  "HAPPY NEW YEAR!"
+  
+end
+
+def countdown_with_sleep number
+  
 end
